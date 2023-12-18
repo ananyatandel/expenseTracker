ee4eaa3c3087e5781dbdacc4f2c5b23ed18ee6bc
Ananya Tandel <anta1515@colorado.edu>
2023-12-17 18:44:36 -0700

    update budget setting view

--- expenseTracker/budgetSettingView.swift
+++ expenseTracker/budgetSettingView.swift
@@ -101,39 +101,46 @@ struct BudgetSettingView: View {
     var body: some View {
         NavigationView {
             ZStack {
-                // Background image
                 Image("quizBackground")
                     .resizable()
                     .aspectRatio(contentMode: .fill)
                     .edgesIgnoringSafeArea(.all)
 
-                // Content
                 ScrollView {
                     VStack(spacing: 20) {
                         Text("Budget Settings")
                             .font(.largeTitle)
                             .fontWeight(.bold)
-                            .padding(.top)
+                            .padding(.top, 60)
+                        
 
-                        GroupBox {
-                            Picker("Select Category", selection: $selectedCategory) {
-                                ForEach(categoryManager.categories, id: \.self) { category in
-                                    Text(category).tag(category)
+                        VStack(alignment: .leading, spacing: 10) {
+                            HStack {
+                                Text("Select Category")
+                                    .foregroundColor(.black)
+                                    .padding(.leading, 10)
+                                Spacer()
+                                Picker("Select Category", selection: $selectedCategory) {
+                                    ForEach(categoryManager.categories, id: \.self) { category in
+                                        Text(category).tag(category)
+                                    }
                                 }
                             }
                             .padding()
+                            .background(Color.white)
+                            .cornerRadius(10)
+                            .pickerStyle(MenuPickerStyle())
 
                             TextField("Budget Amount", text: $budgetAmount)
                                 .keyboardType(.decimalPad)
                                 .padding()
+                                .background(Color.white)
+                                .cornerRadius(10)
 
-                            Picker("Time Frame", selection: $selectedTimeFrame) {
-                                ForEach(TimeFrame.allCases, id: \.self) { timeFrame in
-                                    Text(timeFrame.rawValue).tag(timeFrame)
-                                }
-                            }
-                            .pickerStyle(SegmentedPickerStyle())
-                            .padding()
+                            CustomSegmentedControl(selectedTimeFrame: $selectedTimeFrame)
+                                .padding()
+                                .background(Color.white)
+                                .cornerRadius(10)
 
                             Button("Set Budget") {
                                 setBudget()
@@ -144,42 +151,29 @@ struct BudgetSettingView: View {
                             .foregroundColor(Color.white)
                             .cornerRadius(10)
                         }
-                        .background(Color.white.opacity(0.7))
-                        .cornerRadius(10)
-                        .padding()
+                        .padding(.horizontal)
 
-                        GroupBox {
-                            VStack(alignment: .leading) {
-                                ForEach(budgetManager.budgets) { budget in
-                                    HStack {
-                                        VStack(alignment: .leading) {
-                                            Text(budget.category)
-                                                .font(.headline)
-                                            Text("Amount: \(budget.amount, specifier: "%.2f")")
-                                            Text("Time Frame: \(budget.timeFrame.rawValue)")
-                                        }
-                                        Spacer()
-                                    }
-                                    .padding()
-                                    .background(Color.white)
-                                    .cornerRadius(8)
-                                    .shadow(radius: 4)
-                                    .padding(.horizontal)
-                                }
-                                .onDelete(perform: deleteBudget)
+                        VStack(alignment: .leading, spacing: 10) {
+                            Text("Current Budgets")
+                                .font(.headline)
+                                .padding(.leading, 10)
+
+                            ForEach(budgetManager.budgets) { budget in
+                                BudgetRow(budget: budget)
                             }
+                            .onDelete(perform: deleteBudget)
                         }
-                        .padding()
+                        .padding(.horizontal)
                     }
                 }
             }
-            .navigationBarTitle("Budget Settings", displayMode: .inline)
+            .navigationBarTitle("", displayMode: .inline)
         }
     }
 
     private func setBudget() {
-        if let amount = Double(budgetAmount), !selectedCategory.isEmpty, let timeframe = TimeFrame(rawValue: selectedTimeFrame.rawValue) {
-            let newBudget = Budget(category: selectedCategory, amount: amount, timeFrame: timeframe)
+        if let amount = Double(budgetAmount), !selectedCategory.isEmpty {
+            let newBudget = Budget(category: selectedCategory, amount: amount, timeFrame: selectedTimeFrame)
             budgetManager.setBudget(newBudget)
             resetFields()
         }
@@ -188,11 +182,47 @@ struct BudgetSettingView: View {
     private func resetFields() {
         selectedCategory = ""
         budgetAmount = ""
-        selectedTimeFrame = .weekly
     }
 
     private func deleteBudget(at offsets: IndexSet) {
         budgetManager.budgets.remove(atOffsets: offsets)
+    }
+}
+
+struct CustomSegmentedControl: View {
+    @Binding var selectedTimeFrame: TimeFrame
+
+    var body: some View {
+        HStack {
+            ForEach(TimeFrame.allCases, id: \.self) { timeFrame in
+                Text(timeFrame.rawValue)
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(self.selectedTimeFrame == timeFrame ? Color.blue : Color.gray)
+                    .foregroundColor(Color.white)
+                    .cornerRadius(15)
+                    .onTapGesture {
+                        self.selectedTimeFrame = timeFrame
+                    }
+            }
+        }
+    }
+}
+
+struct BudgetRow: View {
+    let budget: Budget
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(budget.category)
+                .font(.headline)
+            Text("Amount: \(budget.amount, specifier: "%.2f")")
+            Text("Time Frame: \(budget.timeFrame.rawValue)")
+        }
+        .padding()
+        .background(Color.white)
+        .cornerRadius(8)
+        .shadow(radius: 4)
     }
 }
 
@@ -202,4 +232,3 @@ struct BudgetSettingView_Previews: PreviewProvider {
             .environmentObject(CategoryManager())
     }
 }
-
